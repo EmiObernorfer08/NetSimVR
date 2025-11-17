b206b18001bf27260885bdb9bd7f018d87e21685
eoberndorfer08 <emil.oberndorfer08@htl-salzburg.ac.at>
2025-11-17 09:31:56 +0100

    cable function

---
+++ Assets/Emil/scripts/CableFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CableFollow : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+
+    private LineRenderer lr;
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        lr.SetPosition(0, pointA.position);
+        lr.SetPosition(1, pointB.position);
+    }
+}
