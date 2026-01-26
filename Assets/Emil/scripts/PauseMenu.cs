06a20809e59e4dde2ca4f8ee03d83ccb1f286210
VSPAJIC <viktor.spajic07@Htl-salzburg.ac.at>
2026-01-26 08:35:21 +0100

    Lock mouse when pause is active

--- Assets/Emil/scripts/PauseMenu.cs
+++ Assets/Emil/scripts/PauseMenu.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenuUI;
     public GameObject crosshairUI;
+    public GameObject FPC;
+
+    private FirstPersonLook FPL;
+    private float oldSensitivity;
 
     private bool isPaused = false;
 
     void Start()
     {
         pauseMenuUI.SetActive(false);
+        FPL = FPC.GetComponent<FirstPersonLook>(); 
     }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -28,9 +33,12 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         crosshairUI.SetActive(true);
+
+        FPL.sensitivity = oldSensitivity; 
     }
 
     void Pause()
@@ -38,10 +46,15 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
+
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         crosshairUI.SetActive(false);
+
+        oldSensitivity = FPL.sensitivity; 
+        FPL.sensitivity = 0f;             
     }
+
     public void Quit()
     {
         Application.Quit();
